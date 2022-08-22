a563469a1dfb172ec5ae24ac2279478f0616bf68
danstacy1 <danstacy1@gmail.com>
2022-08-22 15:27:09 -0400

    show page fixed.

--- src/components/plants/PlantsIndex.js
+++ src/components/plants/PlantsIndex.js
@@ -1,31 +1,23 @@
 import { useState,useEffect } from 'react'
 import Card from 'react-bootstrap/Card'
 import { Link } from 'react-router-dom'
-
 import LoadingScreen from '../shared/LoadingScreen'
 import { getAllPlants } from '../../api/plants'
 import messages from '../shared/AutoDismissAlert/messages'
-
-
 // PlantsIndex should make a request to the api
 // To get all plants
 // Then display them when it gets them
-
 // style for our card container
 const cardContainerStyle = {
 display: 'flex',
 flexFlow: 'row wrap',
 justifyContent: 'center'
 }
-
 const PlantsIndex = (props) => {
 const [plants, setPlants] = useState(null)
 const [error, setError] = useState(false)
-
 const { msgAlert } = props
-
 console.log('Props in PlantsIndex', props)
-
 useEffect(() => {
     console.log(props)
     getAllPlants()
@@ -37,36 +29,32 @@ useEffect(() => {
                 variant: 'danger',
             })
             setError(true)
-        })    
+        })
     }, [])
-
+    console.log('++++++', plants)
     if (error) {
         return <p>Error!</p>
     }
-
     // If plants haven't been loaded yet, show a loading message
     if (!plants) {
         return <LoadingScreen />
     } else if (plants.length === 0) {
         return <p>No plants yet. Better add some.</p>
     }
-
     const plantCards = plants.map(plant => (
-        <Card style={{ width: '30%', margin: 5}} key={ plant.id }>
-            <Card.Header><Link to={`/greenhome/${plant.id}`}> { plant.name }</Link></Card.Header>
+        <Card style={{ width: '30%', margin: 5}} key={ plant._id }>
+            <Card.Header><Link to={`/greenhome/${plant._id}`}> { plant.name }</Link></Card.Header>
             <Card.Body>
                 <Card.Text>
-                    <Link to={`/greenhome/${plant.id}`}> <img src={`${plant.image}`} height="200" width="auto" /></Link>
+                    <Link to={`/greenhome/${plant._id}`}> <img src={`${plant.image}`} height="200" width="auto" /></Link>
                 </Card.Text>
             </Card.Body>
         </Card>
     ))
-
     return (
         <div style={ cardContainerStyle }>
             { plantCards }
         </div>
     )
 }
-
 export default PlantsIndex
--- src/components/plants/ShowPlant.js
+++ src/components/plants/ShowPlant.js
@@ -1,49 +1,41 @@
 import { useState, useEffect } from 'react'
-
 import { useParams, useNavigate } from 'react-router-dom'
 // useParams will allow us to see our parameters
 // useNavigate will allow us to navigate to a specific page
-
 import { Container, Card, Button } from 'react-bootstrap'
-
 import LoadingScreen from '../shared/LoadingScreen'
 import { getOnePlant, updatePlant, removePlant } from '../../api/plants'
 import messages from '../shared/AutoDismissAlert/messages'
 // import EditPlantModal from './EditPlantModal'
 // import NewToyModal from '../toys/NewToyModal'
 // import ShowToy from '../toys/ShowToy'
-
 // We need to get the plant's id from the parameters
 // Then we need to make a request to the api
 // Then we need to display the results in this component
-
 // we'll use a style object to lay out the toy cards
 const cardContainerLayout = {
     display: 'flex',
     justifyContent: 'center',
     flexFlow: 'row wrap'
 }
-
 const ShowPlant = (props) => {
     const [plant, setPlant] = useState(null)
     const [editModalShow, setEditModalShow] = useState(false)
     // const [toyModalShow, setToyModalShow] = useState(false)
     const [updated, setUpdated] = useState(false)
-
     const { id } = useParams()
+    console.log("here are the props", props)
     const navigate = useNavigate()
     // useNavigate returns a function
     // we can call that function to redirect the user wherever we want to
-
     const { user, msgAlert } = props
     console.log('user in props', user)
     console.log('the plant in showPlant', plant)
     // destructuring to get the id value from our route parameters
-
     useEffect(() => {
         getOnePlant(id)
             .then(res => setPlant(res.data.plant))
-            .catch(err => {                   
+            .catch(err => {
                 msgAlert({
                     heading: 'Error getting plant',
                     message: messages.getPlantsFailure,
@@ -53,15 +45,11 @@ const ShowPlant = (props) => {
                 //navigate back to the home page if there's an error fetching
             })
     }, [updated])
-
-
-
     //****** WE DONT NEED THIS HERE MOVE TO MY SHOW PLANT ********///////
-
     // here we'll declare a function that runs which will remove the plant
     // this function's promise chain should send a message, and then go somewhere
     const removeThePlant = () => {
-        removePlant(user, plant.id)
+        removePlant(user, plant._id)
             // on success send a success message
             .then(() => {
                 msgAlert({
@@ -73,7 +61,7 @@ const ShowPlant = (props) => {
             // then navigate to index
             .then(() => {navigate('/')})
             // on failure send a failure message
-            .catch(err => {                   
+            .catch(err => {
                 msgAlert({
                     heading: 'Error removing plant',
                     message: messages.removePlantFailure,
@@ -85,7 +73,7 @@ const ShowPlant = (props) => {
     // if (pet) {
     //     if (pet.toys.length > 0) {
     //         toyCards = pet.toys.map(toy => (
-    //             <ShowToy 
+    //             <ShowToy
     //                 key={toy._id}
     //                 toy={toy}
     //                 pet={pet}
@@ -96,11 +84,9 @@ const ShowPlant = (props) => {
     //         ))
     //     }
     // }
-
     if (!plant) {
         return <LoadingScreen />
     }
-
     return (
         <>
             <Container className="fluid">
@@ -108,7 +94,7 @@ const ShowPlant = (props) => {
                     <Card.Header>{ plant.name }</Card.Header>
                     <Card.Body>
                         <Card.Text>
-                            <div><small> { plant.image }</small></div>
+                            <div><small><img src={`${plant.image}`}></img></small></div>
                             <div><small>Description: { plant.description }</small></div>
                             <div><small>Light: { plant.light }</small></div>
                             <div><small>Water: { plant.water }</small></div>
@@ -120,11 +106,11 @@ const ShowPlant = (props) => {
             </Container>
                     {/* <Card.Footer>
                         {
-                            pet.owner && user && pet.owner._id === user._id 
+                            pet.owner && user && pet.owner._id === user._id
                             ?
                             <>
-                                <Button onClick={() => setEditModalShow(true)} 
-                                    className="m-2" 
+                                <Button onClick={() => setEditModalShow(true)}
+                                    className="m-2"
                                     variant="warning"
                                 >
                                     Edit Pet
@@ -143,25 +129,24 @@ const ShowPlant = (props) => {
             {/* <Container style={cardContainerLayout}>
                 {toyCards}
             </Container> */}
-            {/* <EditPetModal 
+            {/* <EditPetModal
                 user={user}
-                pet={pet} 
-                show={editModalShow} 
+                pet={pet}
+                show={editModalShow}
                 updatePet={updatePet}
                 msgAlert={msgAlert}
                 triggerRefresh={() => setUpdated(prev => !prev)}
-                handleClose={() => setEditModalShow(false)} 
+                handleClose={() => setEditModalShow(false)}
             /> */}
-            {/* <NewToyModal 
+            {/* <NewToyModal
                 pet={pet}
                 show={toyModalShow}
                 user={user}
                 msgAlert={msgAlert}
                 triggerRefresh={() => setUpdated(prev => !prev)}
-                handleClose={() => setToyModalShow(false)} 
+                handleClose={() => setToyModalShow(false)}
             /> */}
         </>
     )
 }
-
 export default ShowPlant
