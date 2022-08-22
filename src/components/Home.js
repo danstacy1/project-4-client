e5bffe1934e43a89b671dd605ae65c3854c847d6
danstacy1 <danstacy1@gmail.com>
2022-08-22 10:27:41 -0400

    added App.js, Home.js, LoadingScreen, and plants.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,15 @@
+import PlantsIndex from './plants/PlantsIndex'
+
 const Home = (props) => {
 	// const { msgAlert, user } = props
 	console.log('props in home', props)
 
+	const { msgAlert } = props
+
 	return (
 		<>
-			<h2>Home Page</h2>
+			<h2>See the Plants</h2>
+			<PlantsIndex msgAlert={ msgAlert }/>
 		</>
 	)
 }
