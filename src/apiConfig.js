b99565f13d3dca64adf4b9ae9b2bba210f3edaee
dangdo85 <danghdo@gmail.com>
2023-01-06 12:41:12 -0500

    changed process.env.APIURL to backticks

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
-	production: process.env.APIURL,
+	production: `${process.env.APIURL}`,
 	development: 'http://localhost:8000',
 }
 
