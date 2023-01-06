5b328cfcaf7434655426a923e65069818448a749
dangdo85 <danghdo@gmail.com>
2023-01-06 13:02:36 -0500

    hard code backend link

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
-	production: `${process.env.APIURL}`,
+	production: 'https://harlequin-cod-vest.cyclic.app',
 	development: 'http://localhost:8000',
 }
 
